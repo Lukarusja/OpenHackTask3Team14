cbe01b4e9b87f3292034355c45dca67ef1a0aeec
Wladyslaw Bielski <52102804+Lukarusja@users.noreply.github.com>
2022-11-29 19:52:08 +0000

    Update main.cs

--- main.cs
+++ main.cs
@@ -1,66 +1,79 @@
-#r "Newtonsoft.Json"
-
-
-
+using System;
 using System.Net;
-
+using System.Net.Http;
+using System.IO;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
-using Microsoft.Extensions.Primitives;
-
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
 
-
-
-public static async Task<IActionResult> Run(HttpRequest req, ILogger log)
-
+namespace OpenHackTask3
 {
+   // static readonly HttpClient client = new HttpClient();
+    public static class CreateRating
+    {
+        [FunctionName("CreateRating")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequestMessage req,
+            ILogger log)
+        {
 
-log.LogInformation("C# HTTP trigger function processed a request.");
+            string bodyContent = req.Content.ReadAsStringAsync().Result;
+            JObject bodyJson = JObject.Parse(bodyContent);
+            string bodyProductId = bodyJson.GetValue("productId").ToString();
+            string bodyUserId = bodyJson.GetValue("userId").ToString();
+
+            string jsonString = "";
+            //bool ifProductFound = false;
+           // bool ifUserFound = false;
+            using HttpClient client = new HttpClient();
+            var Products = await client.GetAsync("https://serverlessohapi.azurewebsites.net/api/GetProducts");
 
 
+            if (Products != null)
+            {
+                jsonString = Products.Content.ReadAsStringAsync().Result;
+            }
+    
+            JArray json = JArray.Parse(jsonString);
 
-string productId = req.Query["productId"];
+            /*
+            foreach (JObject item in json)
+            {
+                if (item.GetValue("productId").ToString() == bodyProductId)
+                    ifProductFound = true;
 
+                if (item.GetValue("userId").ToString() == bodyUserId)
+                    ifUserFound = true;
+            }
+            */
 
+            if (!json.Contains(bodyUserId) || !json.Contains(bodyProductId))
+            {
+                var message = string.Format("Invalid ProductID or UserID");
+                //return req.CreateErrorResponse(HttpStatusCode.MethodNotAllowed, message);
+            }
 
-string requestBody = String.Empty;
+            if (!req.Method.Equals("POST"))
+            {
+                var message = string.Format("Bad method, use POST instead");
+                //return req.CreateErrorResponse(HttpStatusCode.MethodNotAllowed, message);
+            }
+                log.LogInformation("C# HTTP trigger function processed a request.");
 
-using (StreamReader streamReader = new StreamReader(req.Body))
+            HttpContent content = req.Content;
+            string jsonContent = content.ReadAsStringAsync().Result;
 
-{
+            string responseMessage = string.IsNullOrEmpty("a")
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, shalom. This HTTP triggered function executed successfully.";
 
-requestBody = await streamReader.ReadToEndAsync();
-
+            return new OkObjectResult(responseMessage);
+        }
+    }
 }
-
-dynamic data = JsonConvert.DeserializeObject(requestBody);
-
-productId = productId ?? data?.productId;
-
-
-
-string responseMessage = string.IsNullOrEmpty(productId)
-
-? "Please pass a product id on the query string or in the request body"
-
-: $"The product name for your product id {productId} is Starfruit Explosion";
-
-
-
-if (req.Method == "POST")
-
-{
-
-responseMessage += " and the description is This starfruit ice cream is out of this world";
-
-}
-
- 
-
-
-
-return new OkObjectResult(responseMessage);
-
-}
-
